559bdc5a55427a925997c92cf4276ad7f1a495a8
Benjamin Cederholm <Benjamin.Cederholm@tunstall.com>
2021-07-26 18:07:15 +0200

    3.3

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -1,11 +1,20 @@
-﻿using Entities.Models;
+﻿using Entities.Configuration;
+using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Entities
 {
     public class RepositoryContext : DbContext
     {
-        public RepositoryContext(DbContextOptions options) : base(options) { }
+        public RepositoryContext(DbContextOptions options): base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+        }
+
         public DbSet<Company> Companies { get; set; }
         public DbSet<Employee> Employees { get; set; }
     }
