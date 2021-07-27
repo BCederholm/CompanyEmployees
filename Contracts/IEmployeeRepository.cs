e109a94c3978177426ac61101c7e33dc0b2131c4
Benjamin Cederholm <Benjamin.Cederholm@tunstall.com>
2021-07-27 22:28:41 +0200

    6.2

--- Contracts/IEmployeeRepository.cs
+++ Contracts/IEmployeeRepository.cs
@@ -1,6 +1,11 @@
-﻿namespace Contracts
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Contracts
 {
     public interface IEmployeeRepository
     {
+        IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges);
     }
 }
