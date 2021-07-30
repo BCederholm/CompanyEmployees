0a61e0ff64d07a3438301b7daa16432acbc32267
Benjamin Cederholm <Benjamin.Cederholm@tunstall.com>
2021-07-30 16:21:53 +0200

    15.1

---
+++ CompanyEmployees/ActionFilterAttribute.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees
+{
+    public abstract class ActionFilterAttribute : Attribute, IActionFilter, IFilterMetadata, IAsyncActionFilter, IResultFilter, IAsyncResultFilter, IOrderedFilter
+    {
+        public int Order => throw new NotImplementedException();
+
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            // our code before action executes
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            // our code after action executes
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // execute any code before the action executes
+            var result = await next();
+            // execute any code after the action executes
+        }
+
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
