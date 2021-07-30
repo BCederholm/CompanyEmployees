7976f276c68172ebe8dc05e9cb309edaaea90f7e
Benjamin Cederholm <Benjamin.Cederholm@tunstall.com>
2021-07-30 15:17:56 +0200

    13.1.1

--- Entities/DataTransferObjects/EmployeeForCreationDto.cs
+++ Entities/DataTransferObjects/EmployeeForCreationDto.cs
@@ -11,7 +11,8 @@ namespace Entities.DataTransferObjects
         [MaxLength(30, ErrorMessage = "Maximum length for the Name is 30 characters.")]
         public string Name { get; set; }
         
-        [Required(ErrorMessage = "Age is a required field.")]
+        // [Required(ErrorMessage = "Age is a required field.")]
+        [Range(18, int.MaxValue, ErrorMessage = "Age is required and it can't be lower than 18")]
         public int Age { get; set; }
 
         [Required(ErrorMessage = "Position is a required field.")]
