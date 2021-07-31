bb43ff85cc6dae85e533faa392c2d241c67cf256
Benjamin Cederholm <Benjamin.Cederholm@tunstall.com>
2021-07-31 21:56:18 +0200

    27.3

---
+++ CompanyEmployees/Controllers/AuthenticationController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using CompanyEmployees.ActionFilters;
+using Entities.DataTransferObjects;
+using Entities.Models;
+using LoggerService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyEmployees.Controllers
+{
+
+    [Route("api/authentication")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase // CodeMaze 27
+    {
+        private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
+        private readonly UserManager<User> _userManager;
+
+        public AuthenticationController(ILoggerManager logger, IMapper mapper, UserManager<User> userManager)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> RegisterUser([FromBody] UserForRegistrationDto userForRegistration)
+        {
+            var user = _mapper.Map<User>(userForRegistration);
+
+            var result = await _userManager.CreateAsync(user, userForRegistration.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.TryAddModelError(error.Code, error.Description);
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            if (!userForRegistration.Roles.Any())
+            {
+                _logger.LogInfo("Roles doesn't exist in the registration DTO object, adding the default one.");
+                await _userManager.AddToRoleAsync(user, "Manager");
+            }
+            else
+            {
+                await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+            }
+
+            return StatusCode(201); // Created
+        }
+    }
+}
